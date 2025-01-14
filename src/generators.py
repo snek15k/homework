33d81fb96fee3f9e45ed116a340a94de6fdc7bc2
artem <artemignatuk78@gmail.com>
2025-01-14 09:21:54 +0500

    fixes_last

--- src/generators.py
+++ src/generators.py
@@ -5,7 +5,11 @@ def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[dict
     """
     Фильтрует список транзакций по заданной валюте и возвращает итератор.
     """
-    return (transaction for transaction in transactions if transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency)
+    return (
+        transaction
+        for transaction in transactions
+        if transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency
+    )
 
 
 def transaction_descriptions(transactions: list[dict]) -> Iterator[str]:
@@ -22,4 +26,9 @@ def card_number_generator(start: int, end: int) -> Iterator[str]:
     Генерирует номера банковских карт в формате XXXX XXXX XXXX XXXX в заданном диапазоне.
     """
     for number in range(start, end + 1):
-        yield f"{number:016d}"[:4] + " " + f"{number:016d}"[4:8] + " " + f"{number:016d}"[8:12] + " " + f"{number:016d}"[12:16]
+        yield (
+                f"{number:016d}"[:4] + " " +
+                f"{number:016d}"[4:8] + " " +
+                f"{number:016d}"[8:12] + " " +
+                f"{number:016d}"[12:16]
+        )
