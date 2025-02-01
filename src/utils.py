b516fb8c08a7512ed3ebe5eb74923a58b599b7fc
artem <artemignatuk78@gmail.com>
2025-02-01 14:32:23 +0500

    Commit_control_3

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,22 @@
 import json
+import logging
 import os
 from typing import Any, Dict, List
+
+# Создание логера
+logger = logging.getLogger('utils')
+logger.setLevel(logging.DEBUG)
+
+# Создание обработчика для записи логов в файл
+file_handler = logging.FileHandler('logs/utils.log', mode='w', encoding='utf-8')
+file_handler.setLevel(logging.DEBUG)
+
+# Создание форматера
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+
+# Добавление обработчика к логеру
+logger.addHandler(file_handler)
 
 
 def load_transactions() -> List[Dict[str, Any]]:
@@ -9,23 +25,21 @@ def load_transactions() -> List[Dict[str, Any]]:
 
     :return: Список словарей с данными о транзакциях или пустой список в случае ошибки.
     """
+    logger.debug("Загружаем транзакции из файла.")
+
     file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "operations.json")
 
     if not os.path.exists(file_path):
+        logger.warning(f"Файл не найден: {file_path}")
         return []
 
     try:
         with open(file_path, "r", encoding="utf-8") as file:
             data = json.load(file)
             if isinstance(data, list):
+                logger.info(f"Успешно загружено {len(data)} транзакций.")
                 return data
-    except (json.JSONDecodeError, IOError):
-        pass
+    except (json.JSONDecodeError, IOError) as e:
+        logger.error(f"Ошибка при чтении файла или декодировании JSON: {e}")
 
     return []
-
-
-# Пример использования
-if __name__ == "__main__":
-    transactions: List[Dict[str, Any]] = load_transactions()
-    print(transactions)
